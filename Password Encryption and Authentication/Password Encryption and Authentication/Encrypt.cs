ff9903a99b0f1f6f2d3997b58f9cedf0c82a05a0
johnnyjonesjr90 <54010026+johnnyjonesjr90@users.noreply.github.com>
2020-09-21 20:16:29 -0400

    Primary upload

---
+++ Password Encryption and Authentication/Password Encryption and Authentication/Encrypt.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Password_Encryption_and_Authentication
+{
+    public class Encrypt
+    {
+        //Algorithm
+        // (P+(P-7))*2)
+        //P = position
+
+
+        public List<int> CrossMix(string password)
+        {
+            
+            char[] pass = password.ToCharArray();
+            string[] pass01 = pass.Select(c => c.ToString()).ToArray();
+            int[] intPass = { 65,45,1588,326,44526,12,54,-51,115,5,11,98,247,14,30};
+            for( int u=0; u< pass.Length; u++)
+            {
+                intPass[u] = (Translation(pass01[u].ToUpper())+(Translation(pass01[u].ToUpper())-7)*2);
+            }
+
+            //return intPass;
+            List<int> numbers = new List<int> { 65, 45, 1588, 326, 44526, 12, 54, -51, 115, 5, 11, 98, 247, -14, 30,1253,156,1531,87,96,-4,68,1635,4186,468,351,78454,8561,5,64,8964,98,
+                -615,63,685,4896,1865,468,-7486,153,487,4865,5474,8,46,158,-47,84,96,1,544,86,413,9,76, -4, 68, 1635, 4186, 468, 351, 78454, 8561, 5, 64, 8964, 98, -615, 63, 685, 4896, 1865, 468, 
+                -7486, 153, 487, 4865, 5474, 8, 46, 158, -47, 84, 96, 1, 544, 86, 413, 9, 76 };
+            int i = 0;
+            foreach (int number in intPass)
+            {
+                numbers.Insert(numbers.Count -i,number);
+                i += 3;
+                i++;
+            }
+            
+            return numbers;
+           
+        }
+        public int Translation(string letter)
+        {
+
+            if (letter == "A") return 1;
+            else if (letter == "B") return 2;
+            else if (letter == "C") return 3;
+            else if (letter == "D") return 4;
+            else if (letter == "E") return 5;
+            else if (letter == "F") return 6;
+            else if (letter == "G") return 7;
+            else if (letter == "H") return 8;
+            else if (letter == "I") return 9;
+            else if (letter == "J") return 10;
+            else if (letter == "K") return 11;
+            else if (letter == "L") return 12;
+            else if (letter == "M") return 13;
+            else if (letter == "N") return 14;
+            else if (letter == "O") return 15;
+            else if (letter == "P") return 16;
+            else if (letter == "Q") return 17;
+            else if (letter == "R") return 18;
+            else if (letter == "S") return 19;
+            else if (letter == "T") return 20;
+            else if (letter == "U") return 21;
+            else if (letter == "V") return 22;
+            else if (letter == "W") return 23;
+            else if (letter == "X") return 24;
+            else if (letter == "Y") return 25;
+            else if (letter == "Z") return 26;
+            else if (letter == "1") return 1;
+            else if (letter == "2") return 2;
+            else if (letter == "3") return 3;
+            else if (letter == "4") return 4;
+            else if (letter == "5") return 5;
+            else if (letter == "6") return 6;
+            else if (letter == "7") return 7;
+            else if (letter == "8") return 8;
+            else if (letter == "9") return 9;
+            else if (letter == "0") return 10;
+            else if (letter == "!") return 11;
+            else if (letter == "@") return 12;
+            else if (letter == "#") return 13;
+            else if (letter == "$") return 14;
+            else if (letter == "%") return 15;
+            else if (letter == "&") return 16;
+            else if (letter == "*") return 17;
+            else if (letter == "?") return 18;
+            else return 0;
+        }
+    }
+}
---
+++ Password Encryption and Authentication/Password Encryption and Authentication/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Password_Encryption_and_Authentication
+{
+    class Program
+    {
+
+        /// <summary>
+        /// IN THIS PROGRAM I CRERATED MY OWN SIMPLE HASHING ALGORITHM THAT CHANGES THE WORDS AND SYMBOLS INTO NUMBERS AND SPREADS EACH NUMBER INTO A LIST
+        /// </summary>
+        static void Main()
+        {
+            Dictionary<string, List<int>> account = new Dictionary<string, List<int>>();
+            Encrypt encrypt = new Encrypt();
+            int exit = 0;
+            while (exit == 0)
+            {
+                
+                Console.WriteLine("PASSWORD AUTHENTICATION SYSTEM");
+                Console.WriteLine();
+                Console.WriteLine("Please select one option: \n1.Establish an account \n2.Authenticate a user \n3.Exit the system ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        int enter = 0;
+                        Console.WriteLine("Please enter a username and a Password\n\n Username:");
+                        string userName = Console.ReadLine();
+                        if (account.ContainsKey(userName) ==true)
+                        {
+                            Console.WriteLine("Username is taken");
+                            Console.ReadLine();
+                        }
+                        else if (account.ContainsKey(userName) == false)
+                        {
+                            enter = 1;
+                        }
+                        while (enter == 1)
+                        {
+                            Console.WriteLine("Password(15 characters MAX):");
+                            string passWord = Console.ReadLine();
+                            List<int> test = encrypt.CrossMix(passWord);
+                            Console.WriteLine("Encrypted Password" + string.Join("", test));
+                            account.Add(userName, test);
+                            enter = 0;
+                            Console.ReadLine();
+                        }
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("Sign in");
+                        Console.WriteLine("Username: ");
+                        string verifyUSN = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        string verifyPSW = Console.ReadLine();
+                        List<int> test1 = encrypt.CrossMix(verifyPSW);                   
+                        foreach (KeyValuePair<string, List<int>> element in account)
+                        {
+                            
+                            string name = element.Key;
+                            List<int> age = element.Value;
+                            bool isEqual = Enumerable.SequenceEqual(age, test1);
+                            if (name == verifyUSN && isEqual)
+                            {
+                                Console.WriteLine("Welcome" + name);
+
+                            }
+                        }
+                        Console.Clear();
+                        break;
+                    case 3:
+
+                        exit = 1;
+                        Console.Clear();
+                        break;
+                }
+            }
+
+
+            
+        }
+        
+        
+    }
+}
